c1ddcef43d53f13017d51c294feeb34a27a620c9
Alain Lebreton <alain.lebreton@viaccess-orca.com>
2016-12-27 16:21:00 +0100

    use local xml files

--- test/functional/config/testsConfig.js
+++ test/functional/config/testsConfig.js
@@ -17,58 +17,37 @@ define(function(require) {
                         ads: [
                             {"adsUrl":"",
                              "ExpectedtrackingEvents":{} },
-                            {"adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll.xml",
+                            {"adsUrl":"../ads/xml/mast/preroll.xml",
                              "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1} },
-                            {"adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-image.xml",
+                            {"adsUrl":"../ads/xml/mast/preroll-image.xml",
                              "ExpectedtrackingEvents":{} },
-                            {"adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                            {"adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                              "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"progress":3,"rewind":'x'} }
                         ]
                     },
                     pause: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"pause":1,"resume":1,"progress":3,"rewind":'x'}
                     },
                     mute: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"mute":1,"unmute":1,"progress":3,"rewind":'x'}
                     },
                     closeLinear: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"closeLinear":1,"progress":'x',"midpoint":'x', "firstQuartile":'x',"rewind":'x'}
                     },
                     rewind: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"rewind":4,"progress":3}
                     },
                     fullscreen: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"progress":3,"exitFullscreen":1,"fullscreen":1,"rewind":'x'}
                     },
                     acceptInvitationLinear: {
-                        "adsUrl":"http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30.xml",
+                        "adsUrl":"../ads/xml/mast/preroll-vast30.xml",
                         "ExpectedtrackingEvents":{"creativeView":1,"start":1,"firstQuartile":1,"midpoint":1,"thirdQuartile":1,"complete":1,"progress":3,"acceptInvitationLinear":1,"rewind":'x'}
-                    }
-                },
-
-                // multipleAds test suite specific configuration fields
-                multipleAds: {
-                    //testPageUrl : "http://localhost/csadsplugin/samples/adPodsTestsPlayer",
-                    testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/csadsplugin/samples/adPodsTestsPlayer",
-                    adPod: {
-                        mastUrl: "http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-vast30AdPods.xml",
-                        ads: [  {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_2.mp4", duration : 6000},
-                                {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_4.mp4", duration : 4000}]
-                    },
-                    doubleAdsInVast: {
-                        mastUrl: "http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll-double-vast2.xml",
-                        ads: [  {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_2.mp4", duration : 6000},
-                                {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_4.mp4", duration : 4000}]
-                    },
-                    doubleTriggersInMast: {
-                        mastUrl: "http://cswebplayer.viaccess.fr/adsserver/xml/mast/preroll1-preroll2.xml",
-                        ads: [  {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_2.mp4", duration : 6000},
-                                {media: "http://cswebplayer.viaccess.fr/adsserver/media/vo_ad_4.mp4", duration : 4000}]
                     }
                 }
             }
