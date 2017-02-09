e98c4d2c089451255523442bf59c6f03f6c1bd2e
Alain Lebreton <alain.lebreton@viaccess-orca.com>
2017-02-09 11:27:13 +0100

    clear html5 event

--- test/functional/tests/multipleAds/testSuite.js
+++ test/functional/tests/multipleAds/testSuite.js
@@ -1,7 +1,36 @@
-/**
- TEST_MULTIPLE_ADS_IN_MAST:
+/*
+ * The copyright in this software module is being made available under the BSD License, included
+ * below. This software module may be subject to other third party and/or contributor rights,
+ * including patent rights, and no such rights are granted under this license.
+ *
+ * Copyright (C) 2016 VIACCESS S.A and/or ORCA Interactive
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions
+ *   and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *   conditions and the following disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * - Neither the name of Orange nor the names of its contributors may be used to endorse or promote
+ *   products derived from this software module without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER O
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-Check the adsPlugin behaviour when the mast file embeds more than one ad
+/**
+ TEST_MULTIPLE_ADS:
+
+Check the adsPlayer behaviour when the ad xml file embeds more than one ad
 
  **/
 define(function(require) {
@@ -51,6 +80,9 @@ define(function(require) {
                 return command;
             },
 
+            teardown: function () {
+            },
+
             beforeEach: function (test) {
                 // executes before each test
 
@@ -98,6 +130,8 @@ define(function(require) {
                     .end()
                     // clear the Tracking events
                     .findById("clear_te_button").click()
+                    .end()
+                    .findById("clear_event_html5_button").click()
                     .end()
                 );
             },
