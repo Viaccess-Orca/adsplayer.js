231241f6ce18ea7aa5333404333d2e1e8107b03a
vazquez <a.vazquez.ext@viaccess-orca.com>
2017-02-15 11:08:27 +0100

    Change test URL before merge (reverted from commit 1d8fc6c5d0d59f771d4151b1b82181059619c676)

--- test/functional/config/testsConfig.js
+++ test/functional/config/testsConfig.js
@@ -3,7 +3,7 @@ define(function(require) {
     var _createInstance = function() {
         return {
             // Common tests suite configuration fields
-            testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/CSAdsPlugin-Dev/samples/testsPlayer",        // url of the html page under test
+            testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/CSAdsPlugin-Adrien/samples/testsPlayer",        // url of the html page under test
             //testPageUrl : "http://localhost:8080/samples/testsPlayer",
             streamUrl   : "http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest",// url of the main stream
                                                                                                                         // take care using one with video.currentTime = 0 at the beginning
