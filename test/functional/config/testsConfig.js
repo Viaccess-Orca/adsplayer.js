327064de9b342d427da84437e685d55677d95b31
ledelliou <sophie.ledelliou@viaccess-orca.com>
2017-06-01 10:55:52 +0200

    re-do change on testPageUrl and streamUrl :  use https change CsWebPlayer version to 1.2.1

--- test/functional/config/testsConfig.js
+++ test/functional/config/testsConfig.js
@@ -5,8 +5,8 @@ define(function(require) {
     var _createInstance = function() {
         return {
             // Common tests suite configuration fields
-            testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/CSAdsPlugin-Dev/samples/testsPlayer",
-            streamUrl   : "http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest",// url of the main stream
+            testPageUrl : "https://swag.viaccess.fr/functionnalTests/CSAdsPlugin-Dev/samples/testsPlayer",
+            streamUrl   : "https://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest",// url of the main stream
                                                                                                                         // take care using one with video.currentTime = 0 at the beginning
                                                                                                                         // for the pre-roll tests
             tests : {
