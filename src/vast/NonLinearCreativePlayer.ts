f3897d3505295f757739aeba6a7949f8e7d10480
vazquez <a.vazquez.ext@viaccess-orca.com>
2017-06-22 15:59:01 +0200

    Support minSuggestedDuration for non linears

--- src/vast/NonLinearCreativePlayer.ts
+++ src/vast/NonLinearCreativePlayer.ts
@@ -31,6 +31,7 @@
 import ImagePlayer from '../media/ImagePlayer';
 import EventBus from '../EventBus';
 import Debug from '../Debug';
+import {setTimeout} from "timers";
 
 class NonLinearCreativePlayer {
 
@@ -38,6 +39,9 @@ class NonLinearCreativePlayer {
     private _eventBus: EventBus;
     private _mediaPlayer: ImagePlayer;
     private _onMainVideoPlayingListener:  () => void;
+
+    private _closeDiv: HTMLElement;
+    private _closeStyle: HTMLElement;
 
     /**
      * Initializes the creative player.
@@ -70,27 +74,35 @@ class NonLinearCreativePlayer {
         this.mainVideo = null;
     }
 
-    load(nonLinearAds: any, baseURl: string) : boolean {
+    load(nonLinearAds: any, baseURl: string): boolean {
+        this._removeCloseIcon();
+
+        let nonLinear = nonLinearAds.nonLinear[0];
 
         this._mediaPlayer = new ImagePlayer();
-        this._mediaPlayer.load(baseURl,[{"type": nonLinearAds.nonLinear[0].staticResource.creativeType,
-                                         "uri":  nonLinearAds.nonLinear[0].staticResource.uri}]);
-
+        this._mediaPlayer.load(baseURl,[{"type": nonLinear.staticResource.creativeType,
+                                         "uri":  nonLinear.staticResource.uri}]);
 
         let image : HTMLElement = this._mediaPlayer.getElement();
         image.style.height = "100%";
         image.style.width = "auto";
+        image.style.margin = "auto";
+        image.style.display = "block";
 
         // Add the image to the DOM element
         this.adPlayerContainer.appendChild(image);
 
         // Position image related to the parent positioned div
-        let wImage : number = parseInt(nonLinearAds.nonLinear[0].width);
+        let wImage : number = parseInt(nonLinear.width);
         this.adPlayerContainer.style.position= "absolute";
         this.adPlayerContainer.style.bottom = "5%";
         this.adPlayerContainer.style.left = "calc(50% - " + wImage/2 +"px)";
-        this.adPlayerContainer.style.height = nonLinearAds.nonLinear[0].height+"px";
-        this.adPlayerContainer.style.width = nonLinearAds.nonLinear[0].width+"px";
+        this.adPlayerContainer.style.height = nonLinear.height+"px";
+        this.adPlayerContainer.style.width = nonLinear.width+"px";
+
+        if(nonLinear.minSuggestedDuration) {
+            this._initiateCloseTimer(nonLinear.minSuggestedDuration);
+        }
 
         // Add a handler on the play of main video
         this.mainVideo.addEventListener('playing', this._onMainVideoPlayingListener);
@@ -98,7 +110,80 @@ class NonLinearCreativePlayer {
         return true;
     }
 
-    stop () {
+    _initiateCloseTimer(duration: number) {
+        setTimeout(() => {
+            // Create a close icon
+            this._addCloseIcon();
+        }, duration * 1000);
+    }
+
+    _addCloseIcon() {
+        // Sizes in pixels
+        let iconSize = 20,
+            padding = 3;
+
+        // Create the CSS style
+        let css = `
+            #close-non-linear-icon {
+                position: absolute;
+                top: 5px;
+                right: 5px;
+                cursor: pointer;
+                width: ${iconSize - padding * 2}px;
+                height: ${iconSize - padding * 2}px;
+                padding: ${padding}px;
+                border-radius: ${iconSize / 2}px;
+            }
+            
+            #close-non-linear-icon:hover {
+                background-color: #FF0000;             
+            }
+            
+            #close-non-linear-icon:hover > svg > path {
+                fill: #FFFFFF;
+            }            
+        `;
+
+        // Create the embedded style node
+        this._closeStyle = document.createElement("style");
+        this._closeStyle.appendChild(document.createTextNode(css));
+
+        // Add the CSS to the DOM
+        this.adPlayerContainer.appendChild(this._closeStyle);
+
+        // Create the icon
+        this._closeDiv = document.createElement("div");
+        this._closeDiv.id = "close-non-linear-icon";
+
+        // Embed the icon's SVG
+        this._closeDiv.innerHTML =
+            `<svg viewBox="0 0 24 24">
+                <path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/>
+            </svg>`;
+
+        // Add the icon to the DOM
+        this.adPlayerContainer.appendChild(this._closeDiv);
+
+        // Handle the click event
+        this._closeDiv.onclick = () => {
+            this.stop();
+        };
+    }
+
+    _removeCloseIcon() {
+        if (this._closeDiv) {
+            if (this._closeDiv.remove) {
+                this._closeDiv.remove();
+                this._closeStyle.remove();
+            } else {
+                // remove() may not be supported on IE
+                this.adPlayerContainer.removeChild(this._closeDiv);
+                this.adPlayerContainer.removeChild(this._closeStyle);
+            }
+        }
+    }
+
+    stop() {
         if (this.mainVideo) {
             this.mainVideo.removeEventListener('playing', this._onMainVideoPlayingListener);
         }
@@ -108,20 +193,23 @@ class NonLinearCreativePlayer {
             this.adPlayerContainer.style.height = "0%";
             this.adPlayerContainer.style.width = "0%";
             this.adPlayerContainer.style.display = "none";
+
+            this._removeCloseIcon();
         }
 
         if (this._mediaPlayer) {
-            this.adPlayerContainer.removeChild(this._mediaPlayer.getElement());
+            if (this._mediaPlayer.getElement()) {
+                this.adPlayerContainer.removeChild(this._mediaPlayer.getElement());
+            }
             this._mediaPlayer.delete();
         }
-
     }
 
-    play () {
+    play() {
         //Nothing to do in case of non linear creative
     }
 
-    pause () {
+    pause() {
         //Nothing to do in case of non linear creative
     }
 
